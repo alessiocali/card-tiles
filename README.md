cf8f243c7ff80eeb51698501ced7f21a24a0c686
Alessio <alessiog.cali@hotmail.com>
2022-03-10 19:19:28 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Card Tiles
+Allows you to Drag-n-Drop Cards from you Card Collections onto the game board and automatically create a tile that represents the card. The card face can be iterated by clicking on it (requires [Monk's Active Tile Triggers](https://foundryvtt.com/packages/monks-active-tiles)).
+
+## Todos
+* Map a card 1-to-1 with its Tile representation (for recalling cards to decks).
+* Avoid blocking the board drag-n-drop listener for the purpose of this module.
+* Remove dependency from Monk's plugin (no hard feelings, but less dependencies the better).
+
+## Changelog
+### v0.0.1
+Initial release with bare minimum support.
+
+## Special thanks
+* [u/killercrd](https://www.reddit.com/user/killercrd/) for the [macro](https://www.reddit.com/r/FoundryVTT/comments/sxdwuv/macro_deal_and_place_cards_in_current_scene_as/) that was used as the base for this module 
+* [u/Sherbniz](https://www.reddit.com/user/Sherbniz/) for [spurring the discussion](https://www.reddit.com/r/FoundryVTT/comments/t7ihui/placing_cards_on_a_grid_and_show_them_to_players/) and suggesting/coding the support to Monk's Active Tile Triggers.
---
+++ scripts/module.js
@@ -0,0 +1,86 @@
+Hooks.once('ready', async function() {
+    registerDragDropHandler();
+});
+
+function registerDragDropHandler() {
+    let dragDropConfig = {
+        callbacks: {
+            drop: onCanvasDrop
+        }
+    };
+
+    // Is there a better way than binding to the board? This will prevent any other module from intercepting drag n drop
+    let board = document.getElementById("board");
+    new DragDrop(dragDropConfig).bind(board);
+}
+
+function onCanvasDrop(event) {
+    event.preventDefault();
+
+    let eventData = JSON.parse(event.dataTransfer.getData("text/plain"));
+    if (eventData.type !== "Card") {
+        canvas._dragDrop.callbacks.drop(event);
+        return;
+    }
+
+    let globalPosition = canvas.stage.worldTransform.applyInverse({ x: event.x, y: event.y })
+
+    let cardEventData = {
+        cardCollectionId : eventData.cardsId,
+        cardId : eventData.cardId,
+        x : globalPosition.x,
+        y : globalPosition.y
+    }
+
+    createCardTile(cardEventData);
+}
+
+function createCardTile(cardEventData) {
+    let cardCollection = game.cards.get(cardEventData.cardCollectionId);
+    let card = cardCollection.cards.get(cardEventData.cardId);
+    
+    let monkFlags = {
+        "active" : true,
+        "restriction" : "all",
+        "controlled" : "all",
+        "trigger" : "click",
+        "pertoken" : false,
+        "minrequired" : 0,
+        "chance" : 100,
+        "actions" : [ createCardCycleAction(card) ]
+    };
+
+    let width = card.width || 100;
+    let height = card.height || 100;
+
+    let cardTileData = {
+        x : cardEventData.x - width / 2,
+        y : cardEventData.y - height / 2,
+        width : width,
+        height : height,
+        img : card.back.img,
+        hidden : false,
+        flags : { "monks-active-tiles" : monkFlags }
+    };
+
+    console.log(cardTileData)
+    canvas.scene.createEmbeddedDocuments("Tile", [ cardTileData ]);
+}
+
+function createCardCycleAction(card) {
+    return {
+        "action" : "imagecycle",
+        "data" : {
+            "entity" : "",
+            "imgat" : 1,
+            "files" : buildFacesFiles(card)
+        },
+        "_file-list": "",
+        "id": randomID(16)
+    }
+}
+
+function buildFacesFiles(card) {
+    let allFaces = [ card.back, card.data.faces ].flat();
+    return allFaces.map( face => { return { "id" : randomID(16), "name" : face.img  } } );
+}
