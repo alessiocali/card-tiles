205284169a1e98f6c9ce3dd11c13b89d6ede439b
Alessio <alessiog.cali@hotmail.com>
2022-03-19 11:57:05 +0000

    Added option to pass cards on a "Board" stack.
    * Cards that are played on the canvas are transferred to a global Board
      stack.
    * Cards can be played multiple times on the canvas as there can be only
      one instance of them on the board.
    * This option can be disabled from the settings.

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,6 +4,8 @@ Hooks.once('init', registerSettings);
 
 async function registerSettings() {
     await registerScaling();
+    await registerPassToBoardStack();
+    await registerBoardStackSelector();
 }
 
 async function registerScaling() {
@@ -15,6 +17,39 @@ async function registerScaling() {
         type: Number,
         default: 1.0,
         range: { min: 0.0, max: 10.0, step: 0.1 },
-        filePicker: false
     });
+}
+
+async function registerPassToBoardStack() {
+    await game.settings.register(CardTilesConstants.MODULE_NAME, CardTilesConstants.Settings.PASS_CARDS_TO_BOARD_STACK, {
+        name: game.i18n.localize("CardTiles.Settings.PassCardToBoardStack.Name"),
+        hint: game.i18n.localize("CardTiles.Settings.PassCardToBoardStack.Hint"),
+        scope: "world",
+        config: true,
+        type: Boolean,
+        default: true
+    });
+}
+
+async function registerBoardStackSelector() {
+    await game.settings.register(CardTilesConstants.MODULE_NAME, CardTilesConstants.Settings.BOARD_STACK_NAME, {
+        name: game.i18n.localize("CardTiles.Settings.BoardStack.Name"),
+        hint: game.i18n.localize("CardTiles.Settings.BoardStack.Hint"),
+        scope: "world",
+        config: true,
+        type: String,
+        default: "",
+        choices: buildCardStackSelector
+    });
+}
+
+function buildCardStackSelector() {
+    let selector = { "" : game.i18n.localize("CardTiles.Settings.BoardStack.DefaultBoardStackOption") };
+    
+    let selectorReducer = function (selector, cardStack) {
+        selector[cardStack.id] = cardStack.name;
+        return selector;
+    };
+
+    return Array.from(game.cards).reduce(selectorReducer, selector);
 }
